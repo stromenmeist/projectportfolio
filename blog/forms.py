2ebab7311f4186c47afeee4c048db194afea5cda
stromenmeist <konradkowalke@gmail.com>
2019-09-25 11:50:06 +0200

    finalize tutorial

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 class CommentForm(forms.Form):
     author = forms.CharField(
         max_length=60,
@@ -8,7 +9,7 @@ class CommentForm(forms.Form):
             "placeholder": "Your Name"
         })
     )
-    body = forms.CharField(widget=forms.TextArea(
+    body = forms.CharField(widget=forms.Textarea(
         attrs={
             "class": "form-control",
             "placeholder": "Leave a comment!"
